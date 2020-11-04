94e5c61f54b4819f0e67e6dbb2797704d4eb0450
Returnless <kekeyun728@gmail.com>
2020-11-03 19:51:19 -0500

    .

--- Bilixixi.R
+++ Bilixixi.R
@@ -19,13 +19,18 @@ mydf = read.csv(paste0("docs/",lastup),header = T)
 mydf$viewnum = ifelse(grepl("万",mydf$view),
                       as.numeric(gsub("万", "", mydf$view))*10000,
                       as.numeric(as.character(mydf$view)))
+mydf$text = ifelse(grepl("万",mydf$text),
+                      as.numeric(gsub("万", "", mydf$text))*10000,
+                      as.numeric(as.character(mydf$text)))
 mydf$length = ifelse(grepl(":[0-9]{1,2}:",as.character(mydf$length)),
                      as.character(mydf$length),
                      paste0("00:",as.character(mydf$length)))
 mydf$titlelink = paste0("<a href='https://www.bilibili.com/video/",mydf$link,"' target='_blank'>",mydf$title,"</a>")
 mydf$uplink = paste0("<a href='https://space.bilibili.com/",mydf$space,"' target='_blank'>",mydf$up,"</a>")
 
-vie = mydf%>%filter(!up%in%blacklist)%>%filter(!space%in%blackspace)%>%arrange(-viewnum)
+vie = mydf%>%filter(!up%in%blacklist)%>%filter(!space%in%blackspace)
+vie = vie%>%group_by(titlelink)%>%filter(row_number()==n())%>%arrange(-viewnum)
+
 vie$upload = as.Date(vie$upload)
 vie$lengthnum = lubridate::seconds(lubridate::hms(vie$length))
 vie = vie[,c("titlelink","lengthnum","viewnum","text","upload","uplink","tag")]
