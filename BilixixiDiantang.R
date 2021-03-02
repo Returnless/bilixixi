5c451992f92a695178e5fa22f6a5b5e851d5141e
Returnless <kekeyun728@gmail.com>
2021-03-01 20:58:00 -0500

    .

--- BilixixiDiantang.R
+++ BilixixiDiantang.R
@@ -10,7 +10,7 @@ library(ggplot2)
 blackspace = as.character(scan("docs/LittleBlackRoom.txt"))
 blacklist = c("")
 
-vie = read.csv("docs/BilixixiDiantangOn 2011.csv",header = T)
+vie = read.csv("docs/BilixixiDiantangOn 2102.csv",header = T)
 head(vie)
 vie$titlelink = as.character(vie$titlelink)
 vie$uplink = as.character(vie$uplink)
