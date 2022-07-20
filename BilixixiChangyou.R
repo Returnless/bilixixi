5aaa678fc36b274e8c1decc0cedc57d3e04f1641
Returnless <kekeyun728@gmail.com>
2022-07-19 22:45:55 -0400

    。

--- BilixixiChangyou.R
+++ BilixixiChangyou.R
@@ -7,6 +7,5 @@ library(stringr)
 library(data.table)
 library(ggplot2)
 
-blackspace = as.character(scan("docs/LittleBlackRoom.txt"))
-load("/Users/jiehuixu/Documents/GitHub/bilixixi/docs/BilixixiDiantangAll 2202.RData")
-#vie = read.csv("docs/BilixixiDiantangOn 2111.csv",header = T)
+#blackspace = as.character(scan("docs/LittleBlackRoom.txt"))
+load("/Users/jiehuixu/Documents/GitHub/bilixixi/docs/BilixixiDiantangAll 2207.RData")
